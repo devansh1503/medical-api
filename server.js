0a222b9668e79e76c77b29aad8791431e978fca9
Devansh <guyawsm@gmail.com>
2023-03-23 13:51:54 +0530

    did cors settings

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ connectDB()
 const io = require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:'http://localhost:3000',
+        origin:["http://localhost:3000","https://heartcare247.vercel.app"],
     }
 })
 io.on("connection",(socket)=>{
